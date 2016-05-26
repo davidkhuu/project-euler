20d2f8983e04c96de27bcb466abc4f7fd6783014
David Khuu <davidkhuu.dk@gmail.com>
2016-05-26 17:52:23 -0600

    Clean up Problem 1 and 2
    Just cleaned up some of the comments in the code and displayed the
    output of the function to the console.

--- problem-0001.js
+++ problem-0001.js
@@ -1,7 +1,8 @@
 // Project Euler
 // Problem 1 - Multiples of 3 and 5
-// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-// 
+// If we list all the natural numbers below 10 that are multiples of 3 or 5,
+// we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
 // Author:  David Khuu
@@ -9,7 +10,7 @@
 
 function natural(num) {
   var answer = 0;
-  
+
   for (var i = 1; i <= num; i++) {
     // Check if the current number is divisible by 3 or 5
     if (i % 3 == 0 || i % 5 == 0) {
@@ -17,8 +18,8 @@ function natural(num) {
       answer += i;
     }
   }
-  
+
   return answer;
 }
 
-natural(1000);
+console.log(natural(1000));
